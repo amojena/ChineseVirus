ee0a1c4920d65b2ad452688a174e35883da4a9a1
amojena <am3238@cornell.edu>
2020-04-18 15:31:07 -0400

    Pushing everything

--- apiTest.py
+++ apiTest.py
@@ -1,7 +1,7 @@
 import tweepy
 import json
 import csv
-from time import time
+import time
 
 def load_keys(pathname):
     with open(pathname, "r") as f:
@@ -15,32 +15,54 @@ def setup():
     return auth, api
 
 def test_run(api):
-    tweets = []
+    tweets = set()
     text_query = 'Chinese Virus'
-    count = 3
+    count = 200
     try:
         # Pulling individual tweets from query
-        for tweet in api.search(q=text_query, count=count):
+        for tweet in api.search(q=text_query, count=count, lang='en', until="2020-04-12"):
             #  Adding to list that contains all tweets
-            tweets.append((tweet.text))
-            # print(tweet.text)
+             tweets.add(tweet.text.replace('\n', ' '))
     except BaseException as e:
         print('failed on_status,',str(e))
         time.sleep(3)
         return
     
-    output_to_csv(tweets)
+    filename = "test_tweets.csv"
+    output_to_csv(tweets, filename)
     
 
-def output_to_csv(tweets):
-    with open("test_tweets.csv", 'w') as f:
+def output_to_csv(tweets,filename):
+    with open(filename, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(['Count', 'Tweet'])
-        # writer.writerows(tweets)
         for i, tweet in enumerate(tweets):
             writer.writerow([i, tweet])
+
+def get_trump_tweets():
+    tweets = []
+    username = 'realDonaldTrump'
+    count = 3200
+    last_id = 1241565348765347841 #april 10
+
+    try:
+        for tweet in api.user_timeline(id=username, count=count, max_id=last_id):
+            tweets.append(tweet)
+    except BaseException as e:
+        print('failed on_status,',str(e))
+        time.sleep(3)
+        return
+    
+    filename = "test_trump.csv"
+
+    for t in tweets[-100:]:
+        print(t._json['created_at'], t._t)
+
+
+
 
 if __name__ == "__main__":
     print("Hello Nick!")
     _, api = setup()
-    test_run(api)
+    # test_run(api)
+    get_trump_tweets()
