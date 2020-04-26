15fd3f18ef572afe41259ccdb9743a50721bb9ff
amojena <am3238@cornell.edu>
2020-04-26 15:00:14 -0400

    Querying functionality done, need to add more keywords

--- CSVManager.py
+++ CSVManager.py
@@ -6,6 +6,9 @@ class CSVManager:
 
     def cleanText(self, text):
         return text.replace(',', '').replace('\n', ' ').replace('\"', '')
+    
+    def parseTweet(self, tweet):
+        return tweet[0].replace(',', ''), self.cleanText(tweet[2]), tweet[1][4:].replace(' ', '')
 
     def read(self, filename="tweet_data.csv"):
         print("Getting all queried tweets...")
@@ -14,9 +17,8 @@ class CSVManager:
             with open(filename, 'r') as f:
                 for row in f:
                     tweet = row.split(self.delimeter)
-                    if len(tweet) != 2: raise Exception
-                    date, text = tweet[0].replace(',', ''), self.cleanText(tweet[1])
-                    tweets[text] = date
+                    date, text, rts = self.parseTweet(tweet)
+                    tweets[text] = [date, rts]
         except:
             print(" Error {} does not exist".format(filename))
         print(" {} tweets were saved last session.".format(len(tweets)))
@@ -25,5 +27,6 @@ class CSVManager:
     def write(self, tweets, filename="tweet_data.csv"):
         with open(filename, 'w') as f:
             writer = csv.writer(f)
-            for tweet, date in tweets.items():
-                writer.writerow([date, self.delimeter ,self.cleanText(tweet)])
+            for tweet, info in tweets.items():
+                date, rts = info[0], info[1]
+                writer.writerow([date, self.delimeter, "RTs: {}".format(rts), self.delimeter, self.cleanText(tweet)])
--- TweetManager.py
+++ TweetManager.py
@@ -6,7 +6,7 @@ from CSVManager import CSVManager
 
 class TweetManager:
     def __init__(self):
-        self.queryID = dict() # {textQuery : id of most recent tweet}
+        self.queryID = dict() # {textQuery : id of most recent tweet, # retweets}
         self.setup()
         self.csvMan = CSVManager()
         self.tweets = self.csvMan.read() # {tweet: date}
@@ -19,26 +19,29 @@ class TweetManager:
         auth = tweepy.OAuthHandler(keys["api_key"], keys["api_secret_key"])
         auth.set_access_token(keys["access_token"], keys["access_token_secret"])
         self.api = tweepy.API(auth,wait_on_rate_limit=True)
-        print("Authorization succesful...")
+        print("Authorization succesful...\n")
     
     def getPastQueries(self):
         print("Getting past queries...")
         try:
             with open("past_queries.txt", 'r') as f:
-                for line in f:
-                    query, mostRecentId = line.strip().split('\n')
+                for line in f.readlines():
+                    q = line.split(',')
+                    query, mostRecentId = q[0], q[1].strip()
                     self.queryID[query] = int(mostRecentId)
-                    self.queries.add(query)
+                    print(" Got it")
 
-            if len(self.tweets) < 1 or len(self.queries) == 0: raise Exception
-            print(" There are a total of {} tweets from the queries: {}".format(len(self.tweets), ', '.join(self.queries)))
-        
+            if len(self.tweets) < 1: raise Exception
+            print(" There are a total of {} tweets from the queries: {}".format(len(self.tweets), "".join(self.queryID.keys())))
         except:
             print(" No queries have been made.")
+        print()
 
     def bookmark(self):
         print("Bookmarking {} tweets and {} queries...".format(len(self.tweets), len(self.queryID)))
         self.csvMan.write(self.tweets)
+
+        # Record all queries that have been made with their most recent tweet ID
         with open("past_queries.txt", 'w') as f:
             for query, t_id in self.queryID.items():
                 f.writelines([query, ',', str(t_id)])
@@ -53,16 +56,26 @@ class TweetManager:
         try:
             lastBatch = 0
             while lastBatch < count:
-            # Pulling individual tweets from query
                 query = self.api.search(q=textQuery, count=count, lang='en', since_id=self.queryID[textQuery], tweet_mode='extended')
+
                 for tweet in query:
+                    # Format multi-line tweets to be one-liners
                     t = tweet.full_text.replace('\n', ' ')
                     date = tweet._json['created_at']
 
+                    if "RT" == t[:2]:
+                        # Get original tweet text
+                        t = tweet._json["retweeted_status"]["full_text"]
+
                     if self.tweets.get(t) is None:
+                        # Tweets are not sorted by ID/date, check ID to find largest one (most recent tweet)
                         self.queryID[textQuery] = max(self.queryID[textQuery], tweet.id)
-                        self.tweets[t] = date
+                        self.tweets[t] = [date, 0]
                         lastBatch += 1
+
+                    else:
+                        # Increment retweet count for a tweet
+                        self.tweets[t][1] += 1
 
 
         except BaseException as e:
@@ -75,7 +88,6 @@ class TweetManager:
         print("Bulk querying: {}".format(textQuery))
         try:
             lastId = -1
-            retweet = False
             while len(self.tweets) < count:
                 if lastId != -1:
                     newTweets = self.api.search(q=textQuery, count=count, max_id=str(lastId - 1), tweet_mode='extended')
@@ -92,11 +104,13 @@ class TweetManager:
                     self.queryID[textQuery] = lastId
                 
                 for tweet in newTweets:
+                    # Format multi-line tweets to be one-liners
                     t = tweet.full_text.replace('\n', ' ')
                     date = tweet._json['created_at']
 
                     if "RT" == t[:2]:
-                        t = "RT: " + tweet._json["retweeted_status"]["full_text"]
+                        # Get original tweet text
+                        t = tweet._json["retweeted_status"]["full_text"]
 
                     if self.tweets.get(t) is None:
                         # Tweets are not sorted by ID/date, check ID to find largest one (most recent tweet)
@@ -105,7 +119,12 @@ class TweetManager:
                         # Tweets are not sorted by ID/date, check ID to find smallest one (earliest tweet)
                         lastId = min(lastId, tweet.id)
 
-                        self.tweets[t] = date
+                        self.tweets[t] = [date, 0]
+                    
+                    else:
+                        # Increment retweet count for a tweet
+                        self.tweets[t][1] += 1
+
                 print(" {} tweets left.".format(count - len(self.tweets)))
 
         except BaseException as e:
